77b2f0131037b0d9358ee67b18469e9e6f017c3d
MPqJXDwBc88Y <61336552+mdPT8UyRWfj27c3@users.noreply.github.com>
2022-02-23 14:24:22 +0000

    Update views.py

--- sayches/posts/views.py
+++ sayches/posts/views.py
@@ -115,7 +115,7 @@ def format_posts(request, user, posts):
     return data
 
 
-@cache_page(CACHE_TTL)
+# @cache_page(CACHE_TTL)
 @require_GET
 def posts_data(request):
     if request.is_ajax():
@@ -124,7 +124,7 @@ def posts_data(request):
         if request.user.is_authenticated:
             posts_obj = Post.objects.all().exclude(restrict=True)
         else:
-            posts_obj = Post.objects.all().exclude(restrict=True)[:5]
+            posts_obj = Post.objects.all().exclude(restrict=True)[:10]
         for p in posts_obj:
             posts.append(p)
         data = format_posts(request, user, posts)
