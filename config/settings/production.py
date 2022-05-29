20bed8023a22db666395cae14c230cca86ca93e7
MPqJXDwBc88Y <61336552+mdPT8UyRWfj27c3@users.noreply.github.com>
2022-05-30 00:38:22 +0100

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -104,22 +104,3 @@ INSTALLED_APPS += [
     'storages',
     'django.contrib.sitemaps'
     ]  
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/path/to/django/debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
